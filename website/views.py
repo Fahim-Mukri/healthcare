716f547cd4bd904c8e751272d5504f66c3d099bd
Fahim-Mukri <fahimmukri824@gmail.com>
2024-05-04 17:10:21 +0530

    second msg

--- website/views.py
+++ website/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello world")
+
+
+def dashboard(request):
+    return render(request, "dashboard.html") 
